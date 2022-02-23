c66d1b33cf7b340b1349628ed5a924e0a94df702
JelleSchreursPxl <12002843@student.pxl.be>
2022-02-23 21:28:24 +0100

    api connection try

--- api/Controllers/SeatholderController.cs
+++ api/Controllers/SeatholderController.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using CsvHelper;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Linq;
 
 namespace api.Controllers
 {
---
+++ opdracht1/Seatholder.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace opdracht1
+{
+    class Seatholder
+    {
+        public string Name {get; set;}
+        public string ZipCode {get; set;}
+    }
+}
